3d436bcb52203f872a8787fcf6d5aed085bf344c
James Ferguson <ncjamesferguson@gmail.com>
2017-12-05 04:01:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,21 +11,24 @@ prime = true
   holder = num / 2
 
   while (i <= holder)
+
     x = 0
+
     while (x < holder)
+
       result = i * (holder - x)
       x += 1
 
       if result == num
         prime = false
       end
-      
+
     end
-    
+
     i += 1
-    
+
   end
-  
+
   prime
-  
+
 end
